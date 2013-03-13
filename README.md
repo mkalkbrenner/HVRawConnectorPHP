8162c08289f45b5f5be3d994eb42c27b9b17964a
Markus Kalkbrenner <markus.kalkbrenner@bio.logis.de>
2013-03-13 14:47:32 +0100

    moved source code to github

--- README.md
+++ README.md
@@ -1,4 +1,41 @@
 HVRawConnectorPHP
 =================
 
-Simple PHP library to connect Microsoft HealthVault.
+Simple PHP library to connect
+[Microsoft HealthVault](https://www.healthvault.com/).
+
+
+Requirements
+------------
+
+HVRawConnectorPHP depends on some PHP libraries. All of them can be installed
+with pear:
+
+    pear channel-discover pear.querypath.org
+    pear install querypath/QueryPath
+    pear install Log
+    pear install Net_URL2
+
+Installation and Usage
+----------------------
+
+TODO
+
+Meanwhile you can have a look at the demo_app source code.
+
+
+Demo
+----
+
+The demo_app included in this repository queries a user's HealthVault record
+for all "[Things](http://developer.healthvault.com/pages/types/types.aspx)" and
+dumps the content.
+
+Simply put the HVRawConnectorPHP folder on a webserver and access
+"demo_app/index.php".
+
+
+Licence
+-------
+
+[GPLv2](https://raw.github.com/mkalkbrenner/HVRawConnectorPHP/master/LICENSE.txt).
---
+++ demo_app/index.php
@@ -0,0 +1,61 @@
+<?php
+
+/**
+ * @copyright Copyright 2012-2013 Markus Kalkbrenner, bio.logis GmbH (https://www.biologis.com)
+ * @license GPLv2
+ * @author Markus Kalkbrenner <info@bio.logis.de>
+ */
+
+require_once '../HVRawConnector.php';
+
+$appId = file_get_contents('app.id');
+
+session_start();
+
+try {
+  $hv = new HVRawConnector(
+    $appId,
+    file_get_contents('app.fp'),
+    'app.pem',
+    $_SESSION
+  );
+
+  ob_start();
+
+  $hv->authentifiedWcRequest('GetPersonInfo');
+
+  #var_dump($hv->getRawResponse());
+
+  $personId = $hv->getQueryPathResponse()->find('person-id')->text();
+  $recordId = $hv->getQueryPathResponse()->find('selected-record-id')->text();
+
+  print "person-id: <b>$personId</b><br>selected-record-id: <b>$recordId</b><br><hr>";
+  ob_flush();
+
+  foreach (HVRawConnector::$things as $thing => $thingId) {
+    $hv->authentifiedWcRequest(
+      'GetThings',
+      '3',
+      '<group max="30"><filter><type-id>' . $thingId . '</type-id></filter><format><section>core</section><xml/></format></group>',
+      array('record-id' => $recordId)
+    );
+
+    print "Thing: <b>$thing</b><br>" . htmlentities($hv->getRawResponse()) . "<hr>";
+    ob_flush();
+  }
+}
+catch (HVRawConnectorUserNotAuthentifiedException $e) {
+  printAuthenticationLink();
+}
+catch (HVRawConnectorCredentialTokenExpiredException $e) {
+  printAuthenticationLink();
+}
+
+function printAuthenticationLink() {
+  global $appId;
+
+  print '<a href="' . HVRawConnector::getAuthenticationURL($appId,
+    'http' . (!empty($_SERVER["HTTP_SSL"]) ? 's' : '') . '://' . $_SERVER['HTTP_HOST'] . $_SERVER['REQUEST_URI'],
+    $_SESSION) .
+    '">Authenticate</a>';
+}
