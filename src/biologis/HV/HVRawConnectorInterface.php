6d5ece73f10d39ce68a8d4a282ff3676de3f8400
Markus Kalkbrenner <markus.kalkbrenner@bio.logis.de>
2013-03-26 19:03:34 +0100

    added HVRawConnectorInterface

---
+++ src/biologis/HV/HVRawConnectorInterface.php
@@ -0,0 +1,34 @@
+<?php
+
+/**
+ * @copyright Copyright 2012-2013 Markus Kalkbrenner, bio.logis GmbH (https://www.biologis.com)
+ * @license GPLv2
+ * @author Markus Kalkbrenner <info@bio.logis.de>
+ */
+
+namespace biologis\HV;
+
+
+interface HVRawConnectorInterface {
+
+  public function setCountry($country);
+
+  public function setLanguage($language);
+
+  public function setHealthVaultPlatform($healthVaultPlatform);
+
+  public function connect();
+
+  public function anonymousWcRequest($method, $methodVersion, $info, $additionalHeaders);
+
+  public function authenticatedWcRequest($method, $methodVersion, $info, $additionalHeaders);
+
+  public static function getAuthenticationURL($appId, $redirect, &$session, $healthVaultAuthInstance);
+
+  public static function invalidateSession(&$session);
+
+  public function getRawResponse();
+
+  public function getQueryPathResponse();
+
+}
